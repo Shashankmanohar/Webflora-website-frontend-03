fe5487e3db7979ff56476179cc99245b7e7d0ecf
Shashank Manohar <shashankmanohar1734@gmail.com>
2026-04-30 23:26:34 +0530

    chore: trigger deployment after case studies update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client";
+// Triggering fresh Vercel deployment after Case Studies redesign
 import React from "react";
 import dynamic from "next/dynamic";
 import Hero from "./Components/HeroSection";
